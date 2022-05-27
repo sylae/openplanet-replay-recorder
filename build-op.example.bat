1936b07f7620732320f96ce376843a091c9b86ee
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2022-05-27 19:33:30 +0200

    Update build-op.example.bat

--- build-op.example.bat
+++ build-op.example.bat
@@ -2,9 +2,9 @@
 
 @echo off
 
-IF EXIST Speedometer.op (
-    del Speedometer.op
+IF EXIST ReplayRecorder.op (
+    del ReplayRecorder.op
 )
 :: Set here your path to 7-Zip, including 7z.exe
 SET zip="C:\Program Files\7-Zip\7z.exe"
-%zip% a -mx1 -tzip Speedometer.op info.toml src
+%zip% a -mx1 -tzip ReplayRecorder.op info.toml src
